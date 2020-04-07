e258eb15e27f861c935d83f24a70d2d7ad592ab0
tushar.adlakha@soprasteria.com <tushar.adlakha@soprasteria.com>
2020-04-07 19:19:36 +0530

    TAD - commit of BigXML processing UI

--- bigxmlprocessing-web/src/main/web/src/app/app.component.ts
+++ bigxmlprocessing-web/src/main/web/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'np-app';
+export class AppComponent implements OnInit{
+  title = 'BigXMLProcessing';
+  
+  profileForm:FormGroup;
+  
+  constructor(private fb:FormBuilder){}
+  
+  ngOnInit(){
+  	this.profileForm = this.fb.group({
+  		profile:['']
+  	});
+  }
+  
+  onSelectedFile(event){
+  	console.log(event.target.files);
+  }
+  
+  
+   onSubmit() {
+    console.log("within on submit method" );
+    //this.authService.signIn(this.loginForm.value);
+  }
+  
 }
