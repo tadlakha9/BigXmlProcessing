181702ae3b107bc0acfed57cf4afbb2ae1559f78
TIWARI Swati <swati.tiwari@soprasteria.com>
2020-05-26 19:07:07 +0530

    Swati Tiwari: Committed on behalf of Zahoor: To make Feedback part configurable.

--- bigxmlprocessing-server/src/main/java/com/soprasteria/springboot/utils/SendEmailSSL.java
+++ bigxmlprocessing-server/src/main/java/com/soprasteria/springboot/utils/SendEmailSSL.java
@@ -1,49 +1,57 @@
 package com.soprasteria.springboot.utils;
-
-import javax.mail.*;
+	import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
-public class SendEmailSSL {
+	public class SendEmailSSL {
 
-    public void sendemail(String subject,String content) {
+	    public void sendemail(String subject,String content) {
+	    	Properties prop = new Properties();
+	    	try{
+	    	InputStream input =getClass().getClassLoader().getResourceAsStream("mail.properties");
+	    	   if (input == null) {
+	               System.out.println("Sorry, unable to find config.properties");
+	               return;
+	           }
+	    	  //load a properties file from class path, inside static method
+	         prop.load(input);
 
-        final String username = "bhatzahi@gmail.com";
-        final String password = "ytxjbmyqzpslarqc";
+	     } catch (IOException ex) {
+	         ex.printStackTrace();
+	     }
+	    	
+	        final String username = prop.getProperty("username");
+	        final String password =prop.getProperty("password");
 
-        Properties prop = new Properties();
-		prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "465");
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.socketFactory.port", "465");
-        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+	        Session session = Session.getInstance(prop,
+	                new javax.mail.Authenticator() {
+	                    protected PasswordAuthentication getPasswordAuthentication() {
+	                        return new PasswordAuthentication(username, password);
+	                    }
+	                });
 
-        Session session = Session.getInstance(prop,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
+	        try {
 
-        try {
+	            Message message = new MimeMessage(session);
+	            message.setFrom(new InternetAddress(prop.getProperty("from")));
+	            message.setRecipients(
+	                    Message.RecipientType.TO,
+	                    InternetAddress.parse(prop.getProperty("to"))
+	            );
+	            message.setSubject(subject);
+	            message.setText(content);
 
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("bhatzahi@gmail.com"));
-            message.setRecipients(
-                    Message.RecipientType.TO,
-                    InternetAddress.parse("bhatzaha@gmail.com, swati.tiwari95@gmail.com")
-            );
-            message.setSubject(subject);
-            message.setText(content);
+	            Transport.send(message);
 
-            Transport.send(message);
+	            System.out.println("Done");
 
-            System.out.println("Done");
+	        } catch (MessagingException e) {
+	            e.printStackTrace();
+	        }
+	    }
 
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-    }
-
-}
+	}
